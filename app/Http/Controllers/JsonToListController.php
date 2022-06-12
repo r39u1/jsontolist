2eeb2551d2e9a877ae87d8482277f3e0a13b546c
r39u1 <andrew.regul@mail.ru>
2022-06-12 19:01:04 +0500

    add validation

--- app/Http/Controllers/JsonToListController.php
+++ app/Http/Controllers/JsonToListController.php
@@ -6,13 +6,47 @@ use Illuminate\Http\Request;
 
 class JsonToListController extends Controller
 {
+    protected const URL_REGEX = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+    protected const RGB_SEMICOLONS_REGEX = '/^\(\d+;\d+;\d+\)$/';
+    protected const RGB_COMMAS_REGEX = '/^\(\d+,\d+,\d+\)$/';
+    protected const NUMBER_REGEX = '/^\d+$/';
+    protected const MAX_REGEX = '/^max$/i';
+
     public function convert(Request $request) 
     {
-        $data = json_decode($request->input('json'));
+        $validated = $request->validate([
+            'json' => ['required', 'json'],
+            'depth' => [
+                'nullable',
+                function ($attribute, $value, $fail) {
+                    if (!preg_match(self::NUMBER_REGEX, $value) and 
+                        !preg_match(self::MAX_REGEX, $value) or
+                        strval($value) === '0') {
+                        $fail('The ' . $attribute . ' is invalid.');
+                    }
+                }
+            ],
+            'background' => [
+                'nullable', 
+                function ($attribute, $value, $fail) {
+                    if (!preg_match(self::URL_REGEX, $value) and
+                        !preg_match(self::RGB_SEMICOLONS_REGEX, $value) and
+                        !preg_match(self::RGB_COMMAS_REGEX, $value)) {
+                        $fail('The ' . $attribute . ' is invalid.');
+                    }
+                }
+            ]
+        ]);
+        
+        $data = json_decode($validated['json']);
 
-        $depth = $request->input('depth');
+        $depth = $validated['depth'];
 
-        $background = $this->validate_background($request->input('background'));
+        $background = $validated['background'];
+
+        if (!is_null($background)) {
+            $background = $this->prepare_background($validated['background']);
+        }        
 
         return view('json_to_list', [
             'data' => $data,
@@ -21,20 +55,16 @@ class JsonToListController extends Controller
         ]);
     }
 
-    private function validate_background($background)
+    protected function prepare_background($background)
     {
         $backgroundUrl = null;
         $backgroundRgb = null;
 
-        $urlValidateRegex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
-        $rgbSemicolonsValidateRegex = '/^\(\d+;\d+;\d+\)$/';
-        $rgbCommasValidateRegex = '/^\(\d+,\d+,\d+\)$/';
-
-        if(preg_match($urlValidateRegex, $background)) {
+        if(preg_match(self::URL_REGEX, $background)) {
             $backgroundUrl = $background;
-        } elseif(preg_match($rgbCommasValidateRegex, $background)) {
+        } elseif(preg_match(self::RGB_COMMAS_REGEX, $background)) {
             $backgroundRgb = $background;
-        } elseif(preg_match($rgbSemicolonsValidateRegex, $background)) {
+        } elseif(preg_match(self::RGB_SEMICOLONS_REGEX, $background)) {
             $backgroundRgb = str_replace(';', ',', $background);
         }
 
--- resources/views/send_json.blade.php
+++ resources/views/send_json.blade.php
@@ -7,6 +7,18 @@
     <hr>
     @include('send_json_form', ['method' => 'GET'])
     <hr>
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    <hr>
+    @endif
+    
     @include('send_json_form', ['method' => 'POST'])
     <hr>
 </body>
