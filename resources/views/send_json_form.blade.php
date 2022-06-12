fdc2ddf25861bfaa13ed354090c89a542737d47d
r39u1 <andrew.regul@mail.ru>
2022-06-12 16:19:00 +0500

    add send_json_form_fields view

--- resources/views/send_json_form.blade.php
+++ resources/views/send_json_form.blade.php
@@ -1,11 +1,5 @@
 <form id="send_json_container"
 method="{{ $method }}" action="{{ route('jsonToList') }}">
-    @csrf
-    <label for="json_textarea">JSON</label><br>
-    <textarea id="json_textarea" rows="20" cols="75" name="json"></textarea><br>
-    <label for="depth">Depth</label>
-    <input id="depth" name="depth" type="text"><br>
-    <label for="background">Background</label>
-    <input id="background" name="background" type="text"><br>
+    @include('send_json_form_fields')
     <input type="submit" value="Send JSON {{ $method }}">
 </form>
---
+++ resources/views/send_json_form_fields.blade.php
@@ -0,0 +1,7 @@
+@csrf
+<label for="json_textarea">JSON</label><br>
+<textarea id="json_textarea" rows="20" cols="75" name="json"></textarea><br>
+<label for="depth">Depth</label>
+<input id="depth" name="depth" type="text"><br>
+<label for="background">Background</label>
+<input id="background" name="background" type="text"><br>
