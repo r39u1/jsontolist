369cce9461a9adefc7d8087984a71ed5b3a7e78f
r39u1 <andrew.regul@mail.ru>
2022-06-18 22:26:39 +0500

    apply format

--- public/collapsible_list.js
+++ public/collapsible_list.js
@@ -1,7 +1,7 @@
 var list_buttons = document.getElementsByClassName("list_button");
 
 for (let list_button of list_buttons) {
-    list_button.addEventListener("click", function() {
+    list_button.addEventListener("click", function () {
         this.parentElement.querySelector(".nested").classList.toggle("active");
         this.classList.toggle("minus");
     });
